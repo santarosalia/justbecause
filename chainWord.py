38fde145953b41f547a373070735ff5e9c393cfc
dope <mdc@kakao.com>
2022-07-14 10:21:48 +0900

    a

--- chainWord.py
+++ chainWord.py
@@ -5,6 +5,8 @@ import json
 import requests
 import random
 
+prevWordList = []
+
 
 def inputWord(word):
 
@@ -20,26 +22,37 @@ def inputWord(word):
     xpath2 = '//div[@class="component_keyword has-saving-function"]'
     xpath3 = '//strong[@class="highlight"]'
     try:
-        print("있는 단어")
+
         word = driver.find_elements(By.XPATH, xpath2+xpath3)[0].text
+        print("있는 단어")
+        prevWordList.append(word)
         check = notionCheck(word)
         print('notion 확인 완료')
         if(check[0] == 0):
-            notionInsert(word, 0)
+            notionInsert(word)
             print('notion 작성 완료')
         else:
-            notionUpdate(word, check[1])
+            notionUpdate(check[2], check[1])
             print('notion 수정 완료')
-        wordList = notionReturn(word.strip()[-1])
+        notionReturnWordList = notionReturn(word.strip()[-1])
+        wordList = []
+
+        for notionWord in notionReturnWordList:
+            if(notionWord not in prevWordList):
+                wordList.append(notionWord)
+
         if(len(wordList) > 1):
             i = random.randrange(0, len(wordList))
         elif(len(wordList) == 1):
             i = 0
         else:
+            prevWordList = []
             return '반박 불가'
+        prevWordList.append(wordList[i])
         return wordList[i]
 
-    except Exception:
+    except Exception as e:
+        print(e)
         print("없는 단어")
 
     driver.close()
@@ -74,11 +87,13 @@ def notionCheck(word):
         print(e)
     # dope
     result = json.loads(response.text)
+    pageId = ''
     inputCount = 0
     if(len(result["results"]) > 0):
-        inputCount = result["results"][0]["properties"]["inputCount"]["rich_text"][0]["number"]["content"]
+        inputCount = result["results"][0]["properties"]["inputCount"]["number"]
+        pageId = result["results"][0]["id"]
 
-    return [len(result["results"]), inputCount]
+    return [len(result["results"]), inputCount, pageId]
 
 
 def notionInsert(word):
@@ -97,7 +112,16 @@ def notionInsert(word):
                         }
                     }
                 ]
+            },
+            "inputCount":
+
+                {
+                    "type": "number",
+                    "number": 1
+
             }
+
+
         }
     }
     headers = {
@@ -151,11 +175,49 @@ def notionReturn(firstCharacter):
     return wordList
 
 
-def notionUpdate(word, inputCount):
+def notionUpdate(pageId, inputCount):
+    inputCount += 1
+    url = "https://api.notion.com/v1/pages/"+pageId
+    payload = {
+        "parent": {
+            "database_id": "575a36a00fc94b16b33294410d734b29"
+        },
+        "properties": {
+            "inputCount":
+
+                {
+                    "type": "number",
+                    "number": inputCount
+
+                }
+
+
+        }
+    }
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-02-22",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer secret_NScBJJvTyZmub9pnuX78tkyJErO8RH142EjPplD7bCG"
+    }
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+    except Exception as e:
+        print(datetime.now())
+        print(e)
+    # dope
+
+    return json.loads(response.text)
 
 
 word = input('단어')
 while(True):
     returnWord = inputWord(word)
     print(returnWord)
-    word = input('단어')
+    while(True):
+        word = input('단어')
+        if(word not in prevWordList):
+            break
+        else:
+            print('중복!')
+            continue
